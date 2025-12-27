d889ebf7427ea3beea45c0e9841ac607406ba58c
Adán Luis Quincoces Escalona <adanlqe@gmail.com>
2025-12-27 11:21:05 -0500

    F

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Calculadora/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
